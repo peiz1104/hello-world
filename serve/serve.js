b1ecfa85e82a455720402e788594bb44573cf47e
pmj <peiz1104@users.noreply.github.com>
2021-09-21 09:33:36 +0800

    Update serve.js

--- serve/serve.js
+++ serve/serve.js
@@ -2,7 +2,7 @@ var express = require('express')
 // var {api} = require('./middleware')
 var app = express()
 app.get('/getapi',(req,res)=>{
-  var data = {name:'tongyuxing',age: 18}
+  var data = {name:'tyxing',age: 18}
   var query = req.query.callback+`(${JSON.stringify(data)})` //jsonP
   // res.setHeader('Access-Control-Allow-Origin','*') // cors 解决跨域
   res.send(query);
